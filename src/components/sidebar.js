6fd9c91d9941600765fe6de8223bc17c230abd8a
shilpichak <88437113+shilpichak@users.noreply.github.com>
2021-11-05 03:49:40 +0530

    Update sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,56 +3,56 @@ import React from 'react';
 
 function Sidebar() {
   return (
-	<div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{width: '280px'}}>
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
-      <span class="fs-4">Sidebar</span>
+	<div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{width: '280px'}}>
+    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
+      <span className="fs-4">Sidebar</span>
     </a>
     <hr/>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
+    <ul className="nav nav-pills flex-column mb-auto">
+      <li className="nav-item">
+        <a href="#" className="nav-link active" aria-current="page">
+          <svg className="bi me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
           Home
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
+        <a href="#" className="nav-link text-white">
+          <svg className="bi me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
           Dashboard
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlinkHref="#table"></use></svg>
+        <a href="#" className="nav-link text-white">
+          <svg className="bi me-2" width="16" height="16"><use xlinkHref="#table"></use></svg>
           Orders
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlinkHref="#grid"></use></svg>
+        <a href="#" className="nav-link text-white">
+          <svg className="bi me-2" width="16" height="16"><use xlinkHref="#grid"></use></svg>
           Products
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlinkHref="#people-circle"></use></svg>
+        <a href="#" className="nav-link text-white">
+          <svg className="bi me-2" width="16" height="16"><use xlinkHref="#people-circle"></use></svg>
           Customers
         </a>
       </li>
     </ul>
     <hr/>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" class="rounded-circle me-2" width="32" height="32"/>
+    <div className="dropdown">
+      <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="https://github.com/mdo.png" alt="" className="rounded-circle me-2" width="32" height="32"/>
         <strong>mdo</strong>
       </a>
-      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"/></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+        <li><a className="dropdown-item" href="#">New project...</a></li>
+        <li><a className="dropdown-item" href="#">Settings</a></li>
+        <li><a className="dropdown-item" href="#">Profile</a></li>
+        <li><hr className="dropdown-divider"/></li>
+        <li><a className="dropdown-item" href="#">Sign out</a></li>
       </ul>
     </div>
   </div>
