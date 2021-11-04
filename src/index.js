a163a318916428c5c7c20e2054b0dced6e045338
shilpichak <88437113+shilpichak@users.noreply.github.com>
2021-11-05 03:54:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//import 'bootstrap/dist/css/bootstrap.min.css';  
+import 'bootstrap/dist/css/bootstrap.min.css';  
 import 'bootstrap/dist/js/bootstrap.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
